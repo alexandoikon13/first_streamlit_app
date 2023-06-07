32f9bc3d16af10ad2a3422d199e8d4190c28110d
alexandoikon13 <80966882+alexandoikon13@users.noreply.github.com>
2023-06-07 18:06:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ if streamlit.button('Get Fruit Load List'):
   streamlit.dataframe(my_data_rows)
 
   
-Don't run anything past here while we troubleshoot
+# Don't run anything past here while we troubleshoot
 streamlit.stop()
 
 
